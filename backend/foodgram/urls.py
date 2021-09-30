9549c03a2f609bed69914e1a50dcd757b3639b7d
vavsar <sarcigar@gmail.com>
2021-09-30 15:32:20 +0400

    fix base app

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/users/', include('users.urls')),
-    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/', include('api.urls')),
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        'username',
+        max_length=150,
+        unique=True,
+        help_text=
+            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        validators=[username_validator],
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    first_name = models.CharField('first name', max_length=150)
+    last_name = models.CharField('last name', max_length=150)
+    email = models.EmailField('email address', max_length=254)
